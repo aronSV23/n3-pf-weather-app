f590221f50191fe2b86d951f3898d958b107cdd7
aron <aronsotomarino@gmail.com>
2024-05-10 13:19:16 -0500

    finished

--- src/components/FutureWeatherCard.jsx
+++ src/components/FutureWeatherCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const FutureWeatherCard = ({ fecha, minTemp, maxTemp, iconDir, iconAlt, unit}) => {
     return (
-        <li className='w-[120px] h-[177px] tablet:h-[157px] text-center bg-[#1E213A] flex flex-col justify-center p-3 mb-3 tablet:p-2 tablet:mb-2'>
+        <li className='w-[120px] h-[177px] tablet:h-[157px] text-center bg-[#1E213A] flex flex-col justify-center p-4 mb-3'>
             <h3 className='text-[#E7E7EB] text-[16px] leading-[18.78px] font-medium'>{fecha}</h3>
             <img src={`icons/${iconDir}d.png`} alt={iconAlt} className='m-auto w-[58.44px]' />
             <div className='flex justify-around gap-2  text-[16px] leading-[18.78px] font-medium'>
